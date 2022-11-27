0d1c8d22ec5f10dd120bd706bf7cf74b0a6be398
Xavier Acosta Polo <xavieracosta@ingenieros.com>
2022-11-27 12:27:36 -0500

    agregar nuevos widgets e internacionalizacion

--- form_validation/lib/main.dart
+++ form_validation/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:form_builder_validators/localization/l10n.dart';
 import 'package:form_validation/screens/screens.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        FormBuilderLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const <Locale>[
+        Locale('en', ''), // English, no country code
+        Locale('es', ''), // Spanish, no country code
+      ],
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       home: FormBuilderScreen(),
     );
   }
 }
-
--- form_validation/lib/screens/form_builder_screen.dart
+++ form_validation/lib/screens/form_builder_screen.dart
@@ -7,7 +7,7 @@ class FormBuilderScreen extends StatelessWidget {
   FormBuilderScreen({Key? key}) : super(key: key);
 
   final _formKey = GlobalKey<FormBuilderState>();
-  final logger = Logger();
+  final _logger = Logger();
   final List<String> genderOptions = ['Male', 'Female', 'Other'];
 
   @override
@@ -27,11 +27,11 @@ class FormBuilderScreen extends StatelessWidget {
                   validator: FormBuilderValidators.compose([
                     FormBuilderValidators.required(
                         errorText: 'Este valor es requerido.'),
-                        FormBuilderValidators.email(errorText: 'debe ser email')
+                    FormBuilderValidators.email(errorText: 'debe ser email')
                   ]),
                   decoration: const InputDecoration(label: Text('Full Name')),
                   onChanged: (value) {
-                    logger.i(value);
+                    _logger.i(value);
                     /*
                     logger.v("Verbose log");
                     logger.d("Debug log");
@@ -72,11 +72,116 @@ class FormBuilderScreen extends StatelessWidget {
                           ))
                       .toList(),
                 ),
+                const SizedBox(height: 15),
+                FormBuilderDateTimePicker(
+                  name: 'date',
+                  initialEntryMode: DatePickerEntryMode.calendar,
+                  initialValue: DateTime.now(),
+                  inputType: InputType.date,
+                  decoration: InputDecoration(
+                    labelText: 'Appointment Time',
+                    prefixIcon: const Icon(Icons.calendar_today),
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        _formKey.currentState!.fields['date']?.didChange(null);
+                      },
+                    ),
+                  ),
+                  initialTime: const TimeOfDay(hour: 8, minute: 0),
+                  locale: const Locale.fromSubtags(languageCode: 'es'),
+                ),
+                FormBuilderSlider(
+                  name: 'slider',
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.min(6),
+                  ]),
+                  onChanged: (value) {
+                    _logger.i(value);
+                  },
+                  min: 0.0,
+                  max: 10.0,
+                  initialValue: 7.0,
+                  divisions: 20,
+                  activeColor: Colors.red,
+                  inactiveColor: Colors.pink[100],
+                  decoration: const InputDecoration(
+                    labelText: 'Number of things',
+                  ),
+                ),
+                FormBuilderRangeSlider(
+                  name: 'range_slider',
+                  // validator: FormBuilderValidators.compose([FormBuilderValidators.min(context, 6)]),
+                  onChanged: (value) {
+                    _logger.i('inicio: ${value!.start} - fin: ${value.end}');
+                  },
+                  min: 0.0,
+                  max: 100.0,
+                  initialValue: const RangeValues(4, 7),
+                  divisions: 20,
+                  activeColor: Colors.red,
+                  inactiveColor: Colors.pink[100],
+                  decoration: const InputDecoration(labelText: 'Price Range'),
+                ),
+                FormBuilderCheckbox(
+                  name: 'accept_terms',
+                  initialValue: false,
+                  onChanged: (value) {
+                    _logger.i(value);
+                  },
+                  title: RichText(
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'I have read and agree to the ',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        TextSpan(
+                          text: 'Terms and Conditions',
+                          style: TextStyle(color: Colors.blue),
+                          // Flutter doesn't allow a button inside a button
+                          // https://github.com/flutter/flutter/issues/31437#issuecomment-492411086
+                          /*
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  print('launch url');
+                                },
+                              */
+                        ),
+                      ],
+                    ),
+                  ),
+                  validator: FormBuilderValidators.equal(
+                    true,
+                    errorText:
+                        'You must accept terms and conditions to continue',
+                  ),
+                ),
+                FormBuilderTextField(
+                  autovalidateMode: AutovalidateMode.always,
+                  name: 'age',
+                  decoration: const InputDecoration(
+                    labelText: 'Age',
+                    suffixIcon: true
+                        ? Icon(Icons.error, color: Colors.red)
+                        : Icon(Icons.check, color: Colors.green),
+                  ),
+                  onChanged: (val) {},
+                  // valueTransformer: (text) => num.tryParse(text),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.numeric(),
+                    FormBuilderValidators.max(70),
+                  ]),
+                  // initialValue: '12',
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.next,
+                ),
                 ElevatedButton(
                   child: const Text('Submit'),
                   onPressed: () {
                     if (_formKey.currentState!.saveAndValidate()) {
-                      logger.i(_formKey.currentState!.value);
+                      _logger.i(_formKey.currentState!.value);
                     }
                   },
                 ),
